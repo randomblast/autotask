dc7f1c871794bb4d4e8b38c2be3ee1b1298c40d8
Josh Channings <j.channings@ctisn.com>
2011-01-26 12:46:47 +0000

    Got it reading files, correctly finding comments in PHP

--- autotask.rb
+++ autotask.rb
@@ -2,61 +2,84 @@
 
 class SourceFile
 
-  # Array of comment bodies that we've parsed
-  @comments
+  attr_accessor :comments # Array of comment bodies that we've parsed
 
-  # File pointer
-  @f
+  
+
+  @filename
+  @f # File pointer
+
+  # Comment-finding regexes
+  @@r_comments = {
+    "hash" => /#(.*)$/,
+    "inline_c" => /\/\/(.*)$/,
+    "multiline_c" => /\/\*(.*?)\*\//m
+  }
+
+  # Type finding regexes ("ext" => [regexes])
+  @@r_type_first_lines = {
+    "php" => [/^#!\/.*php.*$/, /^\<\?/],
+    "js" => []
+  }
+
+  # Comment forms to expect in each type
+  @@types = {
+    "php" => ["hash", "inline_c", "multiline_c"]
+  }
+
+  # Pattern to strip non-code text from files
+  @@r_not_code = {
+    #         Start -> <?      ?> -> <?      " -> "      ' -> '             Heredoc
+    "php" => [/^.*?\<\?/m, /\?\>.*?\<\?/m, /\".*?\"/m, /\'.*?\'/m, /<<<([A-Z]+).*\n\\1/m]
+  }
 
   def initialize(filename)
-   @f = File.open(filename, 'r'); 
-  end
+    @comments = Array.new
+    @filename = filename
+    @f = File.open(filename, 'r');
 
-  def type
+    return if !self.type
+    return if !@@types[self.type]
 
-    # Comment-finding regexes
-    r_comments = {
-      "hash" => /#(.*)$/,
-      "inline_c" => /\/\/(.*)$/,
-      "multiline_c" => /\/\*(.*?)\*\//m
-    }
+    code = @f.read
 
-    # Type finding regexes
-    r_type_first_lines = {
-      "php" => [/^#!\/.*php.*$/, /^\<\?/]
-    }
-
-    # Comment forms to expect in each type
-    types = {
-      "php" => ["hash", "inline_c", "multiline_c"]
-    }
-
-    # Test first line
-    @f.seek 0
-    line = @f.gets
-    r_type_first_lines.each do |type|
-      type.each do |t,r|
-        puts r
-#        if puts r.match? line
-#          yield
-#        end
+    if @@r_not_code[self.type]
+      @@r_not_code[self.type].each do |r|
+        code.gsub!(r, "")
       end
     end
 
+    # Add comments to array
+    @@types[self.type].each {|r|
+      code.gsub!(@@r_comments[r]) {|m|
+        @comments << m
+      }
+    }
+  end
+
+  def type
+    @f.seek 0
+    line = @f.gets
+
+    # Test first line
+    @@r_type_first_lines.each {|ext, regexes|
+      regexes.each {|r|
+        return ext if r.match line
+      }
+   }
 
     # Test extension
-  end
-
-  def tasks
+    @@r_type_first_lines.each {|ext, regexes|
+      return ext if File.fnmatch?("*.#{ext}", @filename)
+    }
+    
+    # Nothing matches
+    nil
   end
 
 end
 
-def parse(file)
-  puts file
-end
 
 ARGV.each do |file|
   s = SourceFile.new file
-  puts s.type
 end
